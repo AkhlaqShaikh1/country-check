36a6e3e9249cd3dfb6b1bc5b88fbf228dd63746e
Akhlaq Ahmed Shaikh <akhlaqshaikh03@gmail.com>
2025-12-27 00:02:40 +0500

    updated the build docker file

--- build-docker.sh
+++ build-docker.sh
@@ -12,7 +12,7 @@ echo "You must be logged in to Docker to proceed with the build."
 
 # Build and push Docker image for country-service
 echo "Starting build for country-service..."
-docker buildx build --platform linux/amd64 --tag ghcr.io/intellexal-solutions/country-service:latest --push .
+docker buildx build --platform linux/amd64 --tag ghcr.io/intellexal-solutions/waba-country-check-service:latest --push .
 
 # Notify user that the build is complete
 echo "Build for country-service is complete and the image has been pushed."
